e919b68accd28d5499241fd4e764bac068d241b3
Vachana511 <vachanapranathi@gmail.com>
2024-07-08 10:07:36 +0000

    portfolio website

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Vachana Pranathi",
+  title: "Computer Science student",
+  email: "vachanapranathi@gmail.com",
+  gitHub: "Vachana511",
+  instagram: "va_cha_na",
+  linkedIn: "Vachana Pranathi",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
